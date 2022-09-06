3f89df0a5dcc8f05249467d5595642e9e08e91db
BN <pgbnguyen@gmail.com>
2022-09-05 23:48:26 -0400

    update meta tag for twitter name

--- components/ContainerBlock.js
+++ components/ContainerBlock.js
@@ -30,7 +30,7 @@ export default function ContainerBlock({ children, ...customMeta }) {
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@mannupaaji" />
+        <meta name="twitter:site" content="@PGBNGUYEN99" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
